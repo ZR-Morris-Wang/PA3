94ba77453b94b64b1506c4c546d370a768cc7e37
ZR-Morris-Wang <ppmmlzr@gmail.com>
2024-12-09 10:13:48 +0800

    read input

--- src/cb.cpp
+++ src/cb.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+
 using namespace std;
-int main () {
-	cout << "Hello World" << endl;
+
+struct Edge {
+    int u, v, w;
+};
+
+void buildAdjlist(ifstream& input, vector<vector<Edge>>& adjList, int V, int E, bool directed);
+
+int main (int argc, char* argv []) {
+	char temp = ' ';
+	bool directed = false;
+	int V = 0;
+	int E = 0;
+
+	ifstream input;
+    ofstream output;
+    input.open(argv[1]);
+    output.open(argv[2]);
+
+	input >> temp >> V >> E;
+	directed = (temp == 'd') ? true : false;
+
+	vector<vector<Edge>> adjList(V);
+	buildAdjlist(input, adjList, V, E, directed);
+
+
+	input.close();
+	output.close();
+
 	return 0;
+
+}
+
+
+
+void buildAdjlist(ifstream& input, vector<vector<Edge>>& adjList, int V, int E, bool directed) {
+	for (int i = 0; i < E; ++i) {
+		int u, v, w;
+		input >> u >> v >> w;
+
+		if (directed) {
+			adjList[u].push_back({u, v, w});
+		}
+
+		else if (!directed) {
+			adjList[u].push_back({u, v, w});
+			adjList[v].push_back({v, u, w});
+		}
+	}
 }
