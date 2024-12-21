bfb14530c8e64c8f0c2ec5b26373fef4629b5538
ZR-Morris-Wang <ppmmlzr@gmail.com>
2024-12-21 16:21:43 +0800

    directed v1

--- src/cb_prim.cpp
+++ src/cb_prim.cpp
@@ -8,11 +8,16 @@
 
 using namespace std;
 
+bool cycleFound = true;
+
+enum Color {WHITE, GRAY, BLACK};
+
 struct Vertex {
 	int key;
 	int p;
 	int index;
 	bool inTree;
+	Color color;
 };
 
 struct compareVertex {
@@ -38,6 +43,16 @@ void prim(vector<vector<Edge>>& adjList, priority_queue<Vertex*, vector<Vertex*>
 void printMaxST(vector<Edge>& MaxST);
 int searchParentIndex(const vector<vector<Edge>>& adjList, int u, int parent);
 void notInMaxST(vector<vector<Edge>>& adjList, vector<Edge>& MaxST, ofstream& output);
+void notInMaxSTDirected(vector<vector<Edge>>& adjList, vector<Edge>& MaxST, ofstream& output);
+void breakMinEdge(vector<vector<Edge>>& adjList, vector<Vertex>& vertices, int cycleHead, int cycleTail);
+void dfsInit(vector<vector<Edge>>& adjList, vector<Vertex>& vertices, int V);
+void dfsVisit(vector<vector<Edge>>& adjList, vector<Vertex>& vertices, int u);
+void dfsVisit(vector<vector<Edge>>& adjList, vector<Vertex>& vertices, int u, vector<Edge>& MaxST);
+void dfs(vector<vector<Edge>>& adjList, vector<Vertex>& vertices, int V);
+void dfs(vector<vector<Edge>>& adjList, vector<Vertex>& vertices, int V, vector<Edge>& MaxST);
+void buildDirectedMaxST(vector<vector<Edge>>& adjList, vector<Vertex>& vertices, vector<Edge>& MaxST, int V);
+void allPathSetTrue(vector<vector<Edge>>& adjList);
+
 
 int main (int argc, char* argv []) {
 	char temp = ' ';
@@ -60,17 +75,30 @@ int main (int argc, char* argv []) {
 	vector<Edge> MaxST;
 	
 	// if undirected follow this procedure
-	initVertices(vertices, maxHeap,  V);
-	buildAdjlist(input, adjList, V, E, directed);
-	printAdjList(adjList);		//debug
-	prim(adjList, maxHeap, vertices, MaxST);
-	printMaxST(MaxST);
-	notInMaxST(adjList, MaxST, output);
+	if(!directed) {
+		initVertices(vertices, maxHeap,  V);
+		buildAdjlist(input, adjList, V, E, directed);
+		prim(adjList, maxHeap, vertices, MaxST);
+		notInMaxST(adjList, MaxST, output);
+	} else {
+		
+		buildAdjlist(input, adjList, V, E, directed);
+		allPathSetTrue(adjList);
+		int iteration = 0;
+		while(cycleFound) {
+			iteration++;
+			cout << "Iteration: " << iteration << "\n" << endl;
+			dfs(adjList, vertices, V);
+		}
+		buildDirectedMaxST(adjList, vertices, MaxST, V);
+		notInMaxSTDirected(adjList, MaxST, output);
+	}
+
 	input.close();
 	output.close();
 
 	// if directed follow this procedure
-	
+
 
 	return 0;
 
@@ -101,6 +129,7 @@ void initVertices(vector<Vertex>& vertices, priority_queue<Vertex*, vector<Verte
 		vertices[i].p = -1;
 		vertices[i].index = i;
 		vertices[i].inTree = false;
+		vertices[i].color = WHITE;
 	}
 	maxHeap.push(&vertices[0]);
 }
@@ -145,13 +174,10 @@ void prim(vector<vector<Edge>>& adjList, priority_queue<Vertex*, vector<Vertex*>
 
 }
 
-
-
-
 int searchParentIndex(const vector<vector<Edge>>& adjList, int u, int parent) {
 	for (int i = 0; i < adjList[u].size(); i++) {
 		if (adjList[u][i].v == parent) {
-			cout << "Parent index: " << i << endl;
+			// cout << "Parent index: " << i << endl;
 			return i;
 		}
 	}
@@ -181,6 +207,185 @@ void notInMaxST(vector<vector<Edge>>& adjList, vector<Edge>& MaxST, ofstream& ou
 	}
 }
 
+void notInMaxSTDirected(vector<vector<Edge>>& adjList, vector<Edge>& MaxST, ofstream& output) {
+	ostringstream unseleceted;
+	int removedWeight = 0;
+	bool edgeRemoved = false;
+	for (int i = 0; i < adjList.size(); i++) {
+		for (int j = 0; j < adjList[i].size(); j++) {
+			if (!adjList[i][j].inTree) {
+				adjList[i][j].inTree = true;	// Use .inTree to mark the edge already checked
+				// adjList[adjList[i][j].v][searchParentIndex(adjList, adjList[i][j].v, i)].inTree = true;	// Use .inTree to avoid duplicate edges
+				edgeRemoved = true;
+				unseleceted << adjList[i][j].u << " " << adjList[i][j].v << " " << adjList[i][j].w << endl;
+				removedWeight += adjList[i][j].w;
+			}
+		}
+	}
+
+	if (!edgeRemoved) {
+		output << 0 << endl;
+	} else {
+		output << removedWeight << endl;
+		output << unseleceted.str() << endl;
+	}
+
+}
+
+void allPathSetTrue(vector<vector<Edge>>& adjList) {
+	for (int i = 0; i < adjList.size(); i++) {
+		for (int j = 0; j < adjList[i].size(); j++) {
+			adjList[i][j].inTree = true;
+		}
+	}
+}
+
+void dfsInit(vector<vector<Edge>>& adjList, vector<Vertex>& vertices, int V) {
+	for (int i = 0; i < V; i++) {
+		vertices[i].key = INT_MIN;		// unused here
+		vertices[i].p = -1;
+		vertices[i].index = i;
+		vertices[i].inTree = false;		// unused here
+		vertices[i].color = WHITE;
+	}
+}
+
+void dfsVisit(vector<vector<Edge>>& adjList, vector<Vertex>& vertices, int u) {
+	vertices[u].color = GRAY;
+	for (int i = 0; i < adjList[u].size(); i++) {
+		cout << "u = " << u << endl;
+		cout << "u color: " << vertices[adjList[u][i].u].color << endl;
+		cout << "v = " << adjList[u][i].v << endl;
+		cout << "v color: " << vertices[adjList[u][i].v].color << endl;
+		cout << endl;
+		if (vertices[adjList[u][i].v].color == WHITE && adjList[u][i].inTree) {
+			cycleFound = false;
+			vertices[adjList[u][i].v].p = u;
+			adjList[u][i].inTree = true;
+			if(!cycleFound && vertices[adjList[u][i].v].color != BLACK) {
+				cout << "Row 237: " << endl;
+				dfsVisit(adjList, vertices, adjList[u][i].v);
+			} else {
+				return;
+			}
+		} else if (vertices[adjList[u][i].v].color == GRAY && adjList[u][i].inTree) {
+			adjList[u][i].inTree = true;
+			cout << "---------------- Cycle Break ----------------" << endl;
+			cout << "cycle head: " << adjList[u][i].v << " cycle tail: " << u << endl;
+			cycleFound = true;
+			breakMinEdge(adjList, vertices, adjList[u][i].v, u);		// breakMinEdge (adjList, cycle head, cycle tail)
+			return;
+		} else {
+			cout << "\nPair skipped\n" << endl;
+			continue;
+		}
+
+		if(cycleFound) {
+			return;
+		}
+	}
+	vertices[u].color = BLACK;
+	cout << endl;
+	cout << "u = " << u << " color: " << vertices[u].color << endl;
+	cout << "\n\n" << endl;
+	cycleFound = false;
+	return;
+}
+
+void dfsVisit(vector<vector<Edge>>& adjList, vector<Vertex>& vertices, int u, vector<Edge>& MaxST) {
+	vertices[u].color = GRAY;
+	for (int i = 0; i < adjList[u].size(); i++) {
+		if (vertices[adjList[u][i].v].color == WHITE && adjList[u][i].inTree) {
+			cycleFound = false;
+			vertices[adjList[u][i].v].p = u;
+			adjList[u][i].inTree = true;
+			MaxST.push_back(adjList[u][i]);
+			if(!cycleFound && vertices[adjList[u][i].v].color != BLACK) {
+				dfsVisit(adjList, vertices, adjList[u][i].v);
+			} else {
+				return;
+			}
+		} else {		// The cycles should have all been broken
+			cout << "\nPair skipped\n" << endl;
+			continue;
+		}
+
+		if(cycleFound) {
+			return;
+		}
+	}
+	vertices[u].color = BLACK;
+	cycleFound = false;
+	return;
+}
+
+void dfs(vector<vector<Edge>>& adjList, vector<Vertex>& vertices, int V) {
+	dfsInit(adjList, vertices, V);
+	int root = rand() % V;
+	cout << "Root: " << root << endl;
+	cout << "Row 301: " << endl;
+	if(vertices[root].color != BLACK)	dfsVisit(adjList, vertices, root/*, time*/);
+	if(!cycleFound) {
+		for (int i = 0; i < V; i++) {
+			if (vertices[i].color == WHITE) {
+				cout << "DFS Visit: " << i << endl;
+				cout << "Row 307: " << endl;
+				dfsVisit(adjList, vertices, i);
+				if(cycleFound) {
+					return;
+				}
+			}
+		}
+	}
+	cycleFound = false;
+}
+
+void dfs(vector<vector<Edge>>& adjList, vector<Vertex>& vertices, int V, vector<Edge>& MaxST){
+	cout << "me" << endl;
+	dfsInit(adjList, vertices, V);
+	for (int i = 0; i < V; i++) {
+		if (vertices[i].color == WHITE) {
+			MaxST.push_back(adjList[i][vertices[i].p]);
+			dfsVisit(adjList, vertices, i);
+		}
+	}
+	cout << "Last traversal done" << endl;
+}
+
+void breakMinEdge(vector<vector<Edge>>& adjList, vector<Vertex>& vertices, int cycleHead, int cycleTail) {
+	int u = cycleTail;
+	int v = searchParentIndex(adjList, cycleTail, cycleHead);
+	int minWeight = adjList[u][v].w;
+	Edge minEdge = adjList[u][v];
+	do {
+		cout << "u = " << u << endl;
+		cout << "u color: " << vertices[adjList[u][v].u].color << endl;
+		cout << "v = " << adjList[u][v].v << endl;
+		cout << "v color: " << vertices[adjList[u][v].v].color << endl;
+		int temp;
+		temp = u;
+		u = vertices[adjList[u][v].u].p;
+		v = searchParentIndex(adjList, u, temp);
+		if(adjList[u][v].w < minWeight) {
+			minWeight = adjList[u][v].w;
+			minEdge = adjList[u][v];
+		}
+	} while(adjList[u][v].u != cycleHead);
+	
+	cout << "\nadjList[u][v].u: " << adjList[u][v].u << " cycleHead: " << cycleHead << endl;
+	cout << "minWeight: " << minWeight << endl;
+	
+	adjList[minEdge.u][searchParentIndex(adjList, minEdge.u, minEdge.v)].inTree = false;
+	cout << "removed Edge: " << minEdge.u << " " << minEdge.v << " " << minEdge.w << endl;
+	cout << "--------------------------------------------------\n" << endl;
+	// dfsVisit(adjList, vertices, minEdge.u);
+	
+
+}
+
+void buildDirectedMaxST(vector<vector<Edge>>& adjList, vector<Vertex>& vertices, vector<Edge>& MaxST, int V) {
+	dfs(adjList, vertices, V, MaxST);
+}
 /* ------------Debug Functions------------- */
 void printAdjList(const vector<vector<Edge>>& adjList) {
 	for (int i = 0; i < adjList.size(); ++i) {
@@ -191,7 +396,6 @@ void printAdjList(const vector<vector<Edge>>& adjList) {
 		cout << endl;
 	}
 }
-
 
 void printMaxST(vector<Edge>& MaxST) {
 	cout << "MaxST size: " << MaxST.size() << endl;
--- src/test.cpp
+++ src/test.cpp
@@ -1,20 +1,132 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <random>
+#include <set>
+#include <chrono>
+#include <string>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
-int main() {
-    priority_queue<int, vector<int>, greater<int>> minHeap;
+struct Edge {
+    int from, to, weight;
+    bool operator<(const Edge& other) const {
+        if (from != other.from) return from < other.from;
+        return to < other.to;
+    }
+};
 
-    for (int i = 0; i < 10; ++i) {
-        minHeap.push(rand() % 100);
+vector<int> parent;
+
+int find(int x) {
+    if (parent[x] != x) {
+        parent[x] = find(parent[x]);
+    }
+    return parent[x];
+}
+
+void unite(int x, int y) {
+    parent[find(x)] = find(y);
+}
+
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    if (argc != 5) {
+        cerr << "使用方式: " << argv[0] << " <頂點數量> <邊數量> <圖形類型:u/d> <輸出檔名>" << endl;
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+    int m = atoi(argv[2]);
+    char graph_type = argv[3][0];
+    
+    ofstream fout(argv[4], ios::out | ios::binary);
+    fout.rdbuf()->pubsetbuf(nullptr, 0);  // 關閉緩衝
+
+    if (graph_type == 'u' && (n > 10000 || m > 20000000)) {
+        cerr << "Invalid input for undirected graph!" << endl;
+        return 1;
+    }
+    if (graph_type == 'd' && (n > 5000 || m > 10000)) {
+        cerr << "Invalid input for directed graph!" << endl;
+        return 1;
     }
     
-    while(!minHeap.empty()) {
-        cout << minHeap.top() << " ";
-        minHeap.pop();
+    if (m < n-1) {
+        cerr << "Not enough edges to ensure connectivity!" << endl;
+        return 1;
     }
-    cout << endl;
+
+    // 預分配記憶體
+    parent.reserve(n);
+    vector<Edge> edge_vector;
+    edge_vector.reserve(m);
+    
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    mt19937 gen(seed);
+    uniform_int_distribution<> vertex_dist(0, n-1);
+    uniform_int_distribution<> weight_dist(-100, 100);
+
+    set<Edge> edges;
+    
+    // Initialize union-find
+    parent.resize(n);
+    for(int i = 0; i < n; i++) {
+        parent[i] = i;
+    }
+    
+    // 使用stringstream進行快取輸出
+    stringstream ss;
+    ss << graph_type << '\n' << n << '\n' << m << '\n';
+    
+    // First generate a spanning tree
+    for(int i = 0; i < n-1; ) {
+        int from = vertex_dist(gen);
+        int to = vertex_dist(gen);
+        
+        if(from == to) continue;
+        if(find(from) == find(to)) continue;
+        
+        Edge e{from, to, weight_dist(gen)};
+        if(graph_type == 'u' && from > to) {
+            swap(e.from, e.to);
+        }
+        
+        if(edges.insert(e).second) {
+            unite(from, to);
+            edge_vector.push_back(e);
+            i++;
+        }
+    }
+    
+    // Then add remaining random edges
+    while(edges.size() < m) {
+        int from = vertex_dist(gen);
+        int to = vertex_dist(gen);
+        
+        if(from == to) continue;
+        
+        Edge e{from, to, weight_dist(gen)};
+        if(graph_type == 'u' && from > to) {
+            swap(e.from, e.to);
+        }
+        
+        if(edges.insert(e).second) {
+            edge_vector.push_back(e);
+        }
+    }
+
+    // 批次寫入
+    for(const Edge& e : edge_vector) {
+        ss << e.from << ' ' << e.to << ' ' << e.weight << '\n';
+    }
+    ss << "0\n";
+    
+    fout << ss.str();
+    fout.close();
 
     return 0;
 }
